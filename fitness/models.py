5a66ee9d8c872e3fced0138ad55a373d8ab7e7bf
robertocannella <robertocannella@gmail.com>
2024-07-13 15:24:08 -0400

    before fitness migration

--- fitness/models.py
+++ fitness/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class ExerciseType(models.Model):
+    name = models.CharField(max_length=255)
+
+# Create your models here.
+class ExerciseLog(models.Model):
+    log_id = models.PositiveSmallIntegerField(primary_key=True, unique=True)
+    date = models.DateField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    description = models.TextField()
+
+
+# class ExerciseLogMeta(models.Model):
+#      name = models.CharField(max_length=255)
+
+# class EquipmentType(models.Model):
+#      name = models.CharField(max_length=255)
+
+# class Units(models.Model):
+#      name = models.CharField(max_length=255)
